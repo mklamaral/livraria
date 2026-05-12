3b35457443e3b3115060aa7ccb859b9b2fc7276e
mklamaral <mikael.a.amaral07@gmail.com>
2026-05-12 13:57:04 -0300

    Validando a quantidade de itens em estoque / Gravando o preço do livro no item da compra

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -43,10 +43,11 @@ class CriarEditarCompraSerializer(ModelSerializer):
         fields = ("id", "usuario", "status", "total", "itens")
 
     def create(self, validated_data):
-        itens_data = validated_data.pop("itens")
+        itens = validated_data.pop("itens")
         compra = Compra.objects.create(**validated_data)
-        for item_data in itens_data:
-            ItensCompra.objects.create(compra=compra, **item_data)
+        for item in itens:
+            item["preco_item"] = item["livro"].preco
+            ItensCompra.objects.create(compra=compra, **itens)
         compra.save()
         return compra
 
@@ -55,6 +56,14 @@ class CriarEditarCompraSerializer(ModelSerializer):
         if itens:
             instance.itens.all().delete()
             for item in itens:
+                item["preco_item"] = item["livro"].preco
                 ItensCompra.objects.create(compra=instance, **item)
         instance.save()
         return instance
+
+    def validate(self, data):
+        if data["quantidade"] > data["livro"].quantidade:
+            raise serializers.ValidationError(
+                {"quantidade": "Quantidade solicitada não disponível em estoque"}
+            )
+        return data
